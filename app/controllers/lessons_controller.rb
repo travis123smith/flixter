0dbac05446d3c35b811e650cc707b3df768e25b0
travis smith <travis86smith@gmail.com>
2019-05-14 06:02:42 +0000

    Step 4 Lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
   def show 
   end
 
 private
+
+  def require_authorized_for_current_lesson
+      if current_lesson.section.course.user != current_user
+      #if !current_user.enrollments.pluck(:course_id).include?(current_lesson.section.course.id)
+      redirect_to course_path(current_lesson.section.course), alert: 'Enrollment Required'
+    end
+   end 
 
    helper_method :current_lesson
    def current_lesson
