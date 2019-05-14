f0e0ca365ff90569c3815c380dfa93c0eb1a6ac5
travis smith <travis86smith@gmail.com>
2019-05-14 07:18:38 +0000

    Step 4 Lesson 29

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,5 +6,14 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
-  validates :image, presence: true
+  
+
+  def free?
+    cost.zero?
+  end
+
+    def premium?
+    ! free?
+  end
+
  end
